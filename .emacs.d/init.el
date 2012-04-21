0b331a9815d155827f259da29cdd791173a55354
progd <gprogd@gmail.com>
2012-04-21 17:38:19 +0900

    基本的な設定を追加（「Emacs 実践入門」を参考に）

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,10 @@
+;;; デバッグモードでの起動
+(require 'cl)
+;; Emacsからの質問をy/nで回答する
+;; (fset 'yes-or-no-p 'y-or-n-p)
+;; スタートアップメッセージを非表示
+(setq inhibit-startup-screen t)
+
 ;;; Elisp配置用のディレクトリを作成
 ;; Emacs 23より前のバージョンを利用している方は
 ;; user-emacs-directory変数が未定義のため次の設定を追加
@@ -16,3 +23,121 @@
 
 ;; 引数のディレクトリとそのサブディレクトリをload-pathに追加
 (add-to-load-path "elisp" "conf" "public_repos")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; キーバインドの設定                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; C-hをバックスペースにする
+;; 入力されるキーシーケンスを置き換える
+;; ?\C-?はDELのキーシケンス
+(keyboard-translate ?\C-h ?\C-?)
+
+;;; お勧めのキー操作
+;; C-mにnewline-and-indentを割り当てる。
+(global-set-key (kbd "C-m") 'newline-and-indent)
+;; 折り返しトグルコマンド
+(global-set-key (kbd "C-c l") 'toggle-truncate-lines)
+;; "C-t" でウィンドウを切り替える。初期値はtranspose-chars
+(global-set-key (kbd "C-t") 'other-window)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 環境変数の設定                                         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; パスの設定
+;; (add-to-list 'exec-path "/opt/local/bin")
+;; (add-to-list 'exec-path "/usr/local/bin")
+;; (add-to-list 'exec-path "~/bin")
+
+;;; 文字コードを指定する
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+;;; ファイル名の扱い
+;; Mac OS Xの場合のファイル名の設定
+(when (eq system-type 'darwin)
+  (require 'ucs-normalize)
+  (set-file-name-coding-system 'utf-8-hfs)
+  (setq locale-coding-system 'utf-8-hfs))
+
+;; Windowsの場合のファイル名の設定
+(when (eq window-system 'w32)
+  (set-file-name-coding-system 'cp932)
+  (setq locale-coding-system 'cp932))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; フレームに関する設定                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; モードラインに関する設定
+;; カラム番号も表示
+(column-number-mode t)
+;; ファイルサイズを表示
+(size-indication-mode t)
+;; リージョン内の行数と文字数をモードラインに表示する（範囲指定時のみ）
+;; http://d.hatena.ne.jp/sonota88/20110224/1298557375
+(defun count-lines-and-chars ()
+  (if mark-active
+      (format "%d lines,%d chars "
+              (count-lines (region-beginning) (region-end))
+              (- (region-end) (region-beginning)))
+    ;; これだとエコーエリアがチラつく
+    ;;(count-lines-region (region-beginning) (region-end))
+    ""))
+
+(add-to-list 'default-mode-line-format
+             '(:eval (count-lines-and-chars)))
+
+;;; フレームの初期位置とサイズの設定
+(when window-system
+  (setq initial-frame-alist
+        (append
+         '((top . 0)      ; フレームの Y 位置(ピクセル数)
+           (left . 0)     ; フレームの X 位置(ピクセル数)
+           (width . 90)    ; フレーム幅(文字数)
+           (height . 56)   ; フレーム高(文字数)
+           ) initial-frame-alist)))
+
+;;; タイトルバーにファイルのフルパスを表示
+(setq frame-title-format "%f")
+;; 行番号を常に表示する
+(global-linum-mode t)
+;;; ビープを消す
+(setq ring-bell-function 'ignore)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; インデントの設定                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; タブ文字の表示幅
+;; TABの表示幅。初期値は8
+(setq-default tab-width 4)
+;; インデントにタブ文字を使用しない
+(setq-default indent-tabs-mode nil)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 表示・装飾に関する設定                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; 色
+(when window-system
+  (set-background-color "Black")
+  (set-foreground-color "LightGray")
+  (set-cursor-color "Gray")
+  (set-frame-parameter nil 'alpha 80))
+
+;;; フォントの設定
+(when (eq window-system 'ns)
+  (set-face-attribute 'default nil
+                      :family "Ricty"
+                      :height 140)
+
+  (set-fontset-font
+   nil 'japanese-jisx0208
+   (font-spec :family "Ricty")))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; バックアップとオートセーブ                             ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; バックアップとオートセーブの設定
+;; バックアップとオートセーブファイルを~/.emacs.d/backups/へ集める
+(add-to-list 'backup-directory-alist
+             (cons "." "~/.emacs.d/backups/"))
+(setq auto-save-file-name-transforms
+      `((".*" ,(expand-file-name "~/.emacs.d/backups/") t)))
