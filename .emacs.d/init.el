dfcf9cbca4ff5f97f2aebf0f1623cec17ceed03d
progd <gprogd@gmail.com>
2012-04-29 08:19:11 +0900

    Ruby の設定を追加

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -233,3 +233,25 @@
 ;; Egg (Emacs Got Git)                                    ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'egg nil t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Ruby                                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 括弧の自動挿入 - ruby-electric
+(require 'ruby-electric nil t)
+;; endに対応する行のハイライト - ruby-block
+(when (require 'ruby-block nil t)
+  (setq ruby-block-highlight-toggle t))
+;; インタラクティブRubyを利用する - inf-ruby
+(autoload 'run-ruby "inf-ruby"
+  "Run an inferior Ruby process")
+(autoload 'inf-ruby-keys "inf-ruby"
+  "Set local key defs for inf-ruby in ruby-mode")
+
+;; ruby-mode-hook用の関数を定義
+(defun ruby-mode-hooks ()
+  (inf-ruby-keys)
+  (ruby-electric-mode t)
+  (ruby-block-mode t))
+;; ruby-mode-hookに追加
+(add-hook 'ruby-mode-hook 'ruby-mode-hooks)
