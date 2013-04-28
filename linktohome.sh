b43353be8eb26f6f57845b172eb858c1e4e157ca
progd <progd@debian.(none)>
2013-04-28 15:52:23 +0900

    add shell script for make symlink

---
+++ linktohome.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd ~
+ln -s -f ~/projects/dotfiles/.emacs.d
+ln -s -f ~/projects/dotfiles/.keysnail.d
+ln -s -f ~/projects/dotfiles/.keysnail.js
+ln -s -f ~/projects/dotfiles/.tmux.conf
+ln -s -f ~/projects/dotfiles/.vimperator
+ln -s -f ~/projects/dotfiles/.vimperatorrc
+ln -s -f ~/projects/dotfiles/.zsh.d
+ln -s -f ~/projects/dotfiles/.zshenv
+ln -s -f ~/projects/dotfiles/.zshrc
+ln -s -f ~/projects/dotfiles/global_ignore
