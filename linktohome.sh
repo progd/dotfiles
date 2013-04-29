af96df897d1d3b25dabc387c75e594be98507e9a
progd <progd@debian.(none)>
2013-04-29 13:10:56 +0900

    modiry script for making symlink

--- linktohome.sh
+++ linktohome.sh
@@ -2,12 +2,13 @@
 
 cd ~
 ln -s -f ~/projects/dotfiles/.emacs.d
+ln -s -f ~/projects/dotfiles/.gitconfig
 ln -s -f ~/projects/dotfiles/.keysnail.d
 ln -s -f ~/projects/dotfiles/.keysnail.js
 ln -s -f ~/projects/dotfiles/.tmux.conf
 ln -s -f ~/projects/dotfiles/.vimperator
 ln -s -f ~/projects/dotfiles/.vimperatorrc
+ln -s -f ~/projects/dotfiles/.vimrc
 ln -s -f ~/projects/dotfiles/.zsh.d
 ln -s -f ~/projects/dotfiles/.zshenv
 ln -s -f ~/projects/dotfiles/.zshrc
-ln -s -f ~/projects/dotfiles/global_ignore
