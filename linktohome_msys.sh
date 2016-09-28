17eb5fc3cf0cf15162c6409e9ca766747a96ecb5
progd <gprogd@gmail.com>
2016-09-29 05:50:58 +0900

    update link script

--- linktohome_msys.sh
+++ linktohome_msys.sh
@@ -7,10 +7,12 @@ cd ~
 export MSYS=winsymlinks:nativestrict
 
 ln -s -f $USERPROFILE/projects/dotfiles/.bashrc
-ln -s -f $USERPROFILE/projects/dotfiles/.inputrc
-ln -s -f $USERPROFILE/projects/dotfiles/.minttyrc
 ln -s -f $USERPROFILE/projects/dotfiles/.gitconfig
 ln -s -f $USERPROFILE/projects/dotfiles/.gitignore_global
+ln -s -f $USERPROFILE/projects/dotfiles/.inputrc
+ln -s -f $USERPROFILE/projects/dotfiles/.minttyrc
+ln -s -f $USERPROFILE/projects/dotfiles/.minttyrc-comspec
+ln -s -f $USERPROFILE/projects/dotfiles/.profile
 ln -s -f $USERPROFILE/projects/dotfiles/.tmux.conf
 ln -s -f $USERPROFILE/projects/dotfiles/.vim
 ln -s -f $USERPROFILE/projects/dotfiles/.vimperator
@@ -19,4 +21,8 @@ ln -s -f $USERPROFILE/projects/dotfiles/.vrapperrc
 ln -s -f $USERPROFILE/projects/dotfiles/.zsh.d
 ln -s -f $USERPROFILE/projects/dotfiles/.zshenv
 ln -s -f $USERPROFILE/projects/dotfiles/.zshrc
+ln -s -f $USERPROFILE/projects/dotfiles/cmdrc.bat
 ln -s -f $USERPROFILE/projects/dotfiles/dot.nodoka
+
+# for windows vim
+ln -s -f $USERPROFILE/projects/dotfiles/.vim vimfiles
